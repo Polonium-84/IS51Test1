c9bf8577393ef57f988f1ad404728ee70663d05b
Polo Duarte <happypolito@mail.fresnostate.edu>
2021-04-15 20:15:55 -0700

    finish structure

---
+++ test_1.py
@@ -0,0 +1,19 @@
+
+
+
+"""
+We will have a program determine which of the two salary options is more profitable within a span of 
+10 days (we will also throw in a thrid option that would be set in case both options give the same 
+result). Option 1 gives you $100 dollars a day. Option 2 starts you off with $1 the first day and 
+doubles that amount every day after. There will be two functions to cacluate the pay rate. function1 
+will calculate the amount for the first option, function2 will calculate the amount for the second 
+option. 
+
+function1 will output $100 * 10 days
+function2 will loop 10 times, with each time, doubling the amount and add the amount to the total 
+revenue.
+
+if the amount is equal, we will output to the user "Option 1 and Option 2 pays the same"
+if option1 is better, we output to the user "Option 1 is better"
+if option2 is better, we output to the user "Option 2 is better"
+"""
